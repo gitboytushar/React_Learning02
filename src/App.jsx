0acdc1c1a68d84f26d59284adb7cad1e61aeb2d2
Tushar Verma <123294028+gitboytushar@users.noreply.github.com>
2024-10-17 09:19:28 +0530

    Added and used tailwindcss for first time

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
-function App () {
+function App() {
   const [num, setNum] = useState(0)
+
+  function checkNumValue() {
+    if (num <= 0) {
+      alert("Number cannot be decreased further!!!");
+    } else {
+      setNum(num - 10);
+    }
+  }
 
   return (
     <div>
-      <h3>Number is = {num}</h3>
-      <button onClick={() => setNum(num + 10)}>Increment by 10</button>
-      <button onClick={() => setNum(num - 5)}>Decrement by 5</button>
+      <h3 className='text-8xl text-center'>Number is {num}</h3>
+      <div className='flex align-middle justify-center gap-5 mt-5'>
+        <button onClick={() => setNum(num + 10)}>Increment by 10</button>
+        <button onClick={checkNumValue}>Decrement by 10</button>
+      </div>
     </div>
   )
 }
